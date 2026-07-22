6f93e429f2e596acacad75234d4e0697c8fea776
scholarixglobal-ctrl <samanabran@gmail.com>
2026-07-22 10:06:32 +0400

    feat(web-research): add web.research.result query cache with 7-day TTL

--- sgc_lead_scoring/tests/__init__.py
+++ sgc_lead_scoring/tests/__init__.py
@@ -4,3 +4,4 @@ from . import test_llm_provider
 from . import test_llm_service
 from . import test_lead_scoring
 from . import test_web_research_provider_model
+from . import test_web_research_result_model
